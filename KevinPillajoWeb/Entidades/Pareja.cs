5d18867f27d71f20292f18bd16f454bf5363146a
PILLAJO MONTACHANA KEVIN HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-07-23 19:42:34 -0500

    Add files via upload

---
+++ KevinPillajoWeb/Entidades/Pareja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KevinPillajoExamenWeb.Entitidades
+
+public class Pareja
+
+{
+
+    public int ParejaId { get; set; }
+
+    public string Nombre { get; set; }
+
+    public Estudiante Estudiante { get; set; }
+
+}
